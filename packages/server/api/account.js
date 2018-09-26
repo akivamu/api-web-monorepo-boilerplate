078c1f30fef52108463a328a03a3175f9e564e2a
Akivamu <akivamu@gmail.com>
2018-09-26 15:22:59 +0700

    Remove `password` in response

--- packages/server/api/account.js
+++ packages/server/api/account.js
@@ -2,12 +2,25 @@
 
 const router = require('express').Router()
 const db = require('../db')
+const _ = require('lodash')
+
+function removeSensitiveAccountInfo (accounts) {
+  if (accounts instanceof Array) {
+    return _.map(accounts, function (account) {
+      delete account.password
+      return account
+    })
+  } else {
+    delete accounts.password
+    return accounts
+  }
+}
 
 router.get('/', function (req, res) {
   db.accounts.findAll()
     .then(accounts => {
       res.json({
-        data: accounts
+        data: removeSensitiveAccountInfo(accounts)
       })
     })
 })
